2fbe068ca3bc54ab4587815a3c8c05c1dd44c030
Shady-AbouElmakarem <shadyabouelmakarem@gmail.com>
2017-05-11 07:45:45 +0200

    some code clean up on buses_live_feed.js

--- public/js/buses_live_feed.js
+++ public/js/buses_live_feed.js
@@ -9,11 +9,24 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
-var ref, route, start, end, midLat, midLng, date, interval;
+var ref, route, start, end, midLat, midLng, date, interval, points=[];
 
+function receivedDate(x){
+  var receivedDate = new Date(x);
+  var dd = receivedDate.getDate();
+  var mm = receivedDate.getMonth()+1;
+  var yyyy = receivedDate.getFullYear();
+  if(dd<10) {
+    dd='0'+dd
+  }
+  if(mm<10) {
+    mm='0'+mm
+  }
+  receivedDate = dd + "/" + mm +"/" + yyyy;
+  return receivedDate;
+}
 
 // Google Maps
-var points = [];
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -23,45 +36,74 @@ function initMap() {
     center: {lat: 29.976650, lng: 30.948297}
   });
 
+  // New bus selected
+  $("select[name=bus]").change(function(){
+    $("input[name=date]").prop('disabled', false);
+    $('input[name=interval]').prop('disabled', false);
+    bus = $(this).val();
+    date = $("input[name=date]").val();
+    interval = $('input[name=interval]:checked').val();
+    if (route)
+    {
+      route.setMap(null);
+      points = [];
+    }
+    ref = firebase.database().ref("live/" + bus);
+    ref.on("value", function(snapshot) {
+      for (var key in snapshot.val()) {
+        var checkDate = receivedDate(snapshot.val()[key]["timeStamp"]*1000);
+        if(checkDate == date){
+          var hours = new Date(snapshot.val()[key]["timeStamp"]*1000).getHours();
+          hours = (hours+24-2)%24;
+          var checkInterval = 'am';
+          if(hours>12)
+          {
+            checkInterval='pm';
+          }
+          if(checkInterval == interval){
+            points.push({lat:snapshot.val()[key]["lat"], lng: snapshot.val()[key]["lng"]});
+          }
+        }
+      }
+      if (points.length > 0){
+        if(route){
+          route.setMap(null);
+        }
+        route = new google.maps.Polyline({
+          path: points,
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.7,
+          strokeWeight: 3
+        });
+        start = points[0];
+        end = points[points.length-1];
+        midLat = (start.lat + end.lat)/2;
+        midLng = (start.lng + end.lng)/2;
+        map.setCenter(new google.maps.LatLng(midLat, midLng));
+        map.setZoom(11);
+        route.setMap(map);
+      }
+      else{
+        alert('No route found');
+      }
+    });
+  });
+
+  // New date selected
   $("input[name=date]").change(function(){
     date = $(this).val();
-    if (date == "live"){
-      date = new Date();
-      var dd = date.getDate();
-      var mm = date.getMonth()+1;
-      var yyyy = date.getFullYear();
-      if(dd<10) {
-        dd='0'+dd
-      }
-      if(mm<10) {
-        mm='0'+mm
-      }
-      date = dd + "/" + mm +"/" + yyyy;
-    }
-    interval = $('input[name=interval]:checked').val();
 
     if (route)
     {
       route.setMap(null);
       points = [];
     }
-    ref = firebase.database().ref("live/"+$('#collapse > div > div:nth-child(2) > div > select').val());
     ref.on("value", function(snapshot) {
       for (var key in snapshot.val()) {
-        var receivedDate = new Date(snapshot.val()[key]["timeStamp"]*1000);
-        var dd = receivedDate.getDate();
-        var mm = receivedDate.getMonth()+1;
-        var yyyy = receivedDate.getFullYear();
-        if(dd<10) {
-          dd='0'+dd
-        }
-        if(mm<10) {
-          mm='0'+mm
-        }
-        checkDate = dd + "/" + mm +"/" + yyyy;
+        var checkDate = receivedDate(snapshot.val()[key]["timeStamp"]*1000);
         if(checkDate == date){
-          var hours = receivedDate.getHours();
-          var hours = (hours+24-2)%24;
+          var hours = new Date(snapshot.val()[key]["timeStamp"]*1000).getHours();
+          hours = (hours+24-2)%24;
           var checkInterval = 'am';
           if(hours>12)
           {
@@ -73,6 +115,9 @@ function initMap() {
         }
       }
       if (points.length > 0){
+        if(route){
+          route.setMap(null);
+        }
         route = new google.maps.Polyline({
           path: points,
           strokeColor: '#FF0000',
@@ -89,30 +134,12 @@ function initMap() {
       }
       else{
         alert('No route found');
-        if(route){
-          route.setMap(null);
-        }
-        points = [];
-
       }
     });
   });
 
+  // New interval selected
   $('input[name=interval]').change(function(){
-    date = $("input[name=date]").val();
-    if (date == "live"){
-      date = new Date();
-      var dd = date.getDate();
-      var mm = date.getMonth()+1;
-      var yyyy = date.getFullYear();
-      if(dd<10) {
-        dd='0'+dd
-      }
-      if(mm<10) {
-        mm='0'+mm
-      }
-      date = dd + "/" + mm +"/" + yyyy;
-    }
     interval = $('input[name=interval]:checked').val();
 
     if (route)
@@ -120,23 +147,12 @@ function initMap() {
       route.setMap(null);
       points = [];
     }
-    ref = firebase.database().ref("live/"+$('#collapse > div > div:nth-child(2) > div > select').val());
     ref.on("value", function(snapshot) {
       for (var key in snapshot.val()) {
-        var receivedDate = new Date(snapshot.val()[key]["timeStamp"]*1000);
-        var dd = receivedDate.getDate();
-        var mm = receivedDate.getMonth()+1;
-        var yyyy = receivedDate.getFullYear();
-        if(dd<10) {
-          dd='0'+dd
-        }
-        if(mm<10) {
-          mm='0'+mm
-        }
-        checkDate = dd + "/" + mm +"/" + yyyy;
+        var checkDate = receivedDate(snapshot.val()[key]["timeStamp"]*1000);
         if(checkDate == date){
-          var hours = receivedDate.getHours();
-          var hours = (hours+24-2)%24;
+          var hours = new Date(snapshot.val()[key]["timeStamp"]*1000).getHours();
+          hours = (hours+24-2)%24;
           var checkInterval = 'am';
           if(hours>12)
           {
@@ -148,6 +164,9 @@ function initMap() {
         }
       }
       if (points.length > 0){
+        if(route){
+          route.setMap(null);
+        }
         route = new google.maps.Polyline({
           path: points,
           strokeColor: '#FF0000',
@@ -164,89 +183,8 @@ function initMap() {
       }
       else{
         alert('No route found');
-        if(route){
-          route.setMap(null);
-        }
-        points = [];
-
       }
     });
-  });
-  $('#collapse > div > div:nth-child(2) > div > select').change(function(){
-      $("input[name=date]").prop('disabled', false);
-      $('input[name=interval]').prop('disabled', false);
-      date = $("input[name=date]").val();
-      if (date == "live"){
-        date = new Date();
-        var dd = date.getDate();
-        var mm = date.getMonth()+1;
-        var yyyy = date.getFullYear();
-        if(dd<10) {
-          dd='0'+dd
-        }
-        if(mm<10) {
-          mm='0'+mm
-        }
-        date = dd + "/" + mm +"/" + yyyy;
-      }
-      interval = $('input[name=interval]:checked').val();
-      if (route)
-      {
-        route.setMap(null);
-        points = [];
-      }
-      points =[];
-      ref = firebase.database().ref("live/"+$(this).val());
-      ref.on("value", function(snapshot) {
-        for (var key in snapshot.val()) {
-          var receivedDate = new Date(snapshot.val()[key]["timeStamp"]*1000);
-          var dd = receivedDate.getDate();
-          var mm = receivedDate.getMonth()+1;
-          var yyyy = receivedDate.getFullYear();
-          if(dd<10) {
-            dd='0'+dd
-          }
-          if(mm<10) {
-            mm='0'+mm
-          }
-          checkDate = dd + "/" + mm +"/" + yyyy;
-          if(checkDate == date){
-            var hours = receivedDate.getHours();
-            var hours = (hours+24-2)%24;
-            var checkInterval = 'am';
-            if(hours>12)
-            {
-              checkInterval='pm';
-            }
-            if(checkInterval == interval){
-              points.push({lat:snapshot.val()[key]["lat"], lng: snapshot.val()[key]["lng"]});
-            }
-          }
-        }
-        if (points.length > 0){
-          route = new google.maps.Polyline({
-            path: points,
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.7,
-            strokeWeight: 3
-          });
-          start = points[0];
-          end = points[points.length-1];
-          midLat = (start.lat + end.lat)/2;
-          midLng = (start.lng + end.lng)/2;
-          map.setCenter(new google.maps.LatLng(midLat, midLng));
-          map.setZoom(11);
-          route.setMap(map);
-        }
-        else{
-          alert('No route found');
-          if(route){
-            route.setMap(null);
-          }
-          points = [];
-
-        }
-      });
   });
 }
 
